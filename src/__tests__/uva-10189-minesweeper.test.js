cbcceefb56daf4911b42a36a26c979da1eb1dfd3
Taylor Everding <taylor1791@users.noreply.github.com>
2022-03-06 14:07:37 -0700

    Add UVA 10189

---
+++ src/__tests__/uva-10189-minesweeper.test.js
@@ -0,0 +1,9 @@
+var uva10189 = require("../uva-10189-minesweeper");
+
+describe.skip("UVA 10189: Minesweeper", () => {
+  it("returns the field for sample 1", () => {
+    const field = "*...\n....\n.*..\n....";
+
+    expect(uva10189(field)).toBe("*100\n2210\n1*10\n1110");
+  });
+});
