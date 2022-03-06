9a0933427b6c93f6cbfa000d4103abfbed10f0f4
Taylor Everding <taylor1791@users.noreply.github.com>
2022-03-05 19:09:01 -0700

    Add UVA00100

---
+++ src/__tests__/uva-00100-the-3n1-problem.test.js
@@ -0,0 +1,7 @@
+var uva00100 = require("../uva-00100-the-3n1-problem");
+
+describe("UVA 00100: The 3n + 1 Problem", () => {
+  it("returns 20 for sample 1: i = 1, j = 10", () => {
+    expect(uva00100(1, 10)).toBe(20);
+  });
+});
